885ee5148e72d08af886df9eec6ad1acc987ba5e
Selen GORA <me@selengora.com>
2012-09-05 11:38:20 +0300

    2012-09-05

--- js/init.js
+++ js/init.js
@@ -27,36 +27,37 @@ $(function(){
 				fill: attributes.fill
 			}, 300);
 		})
-//		.click(function(){
-//			document.location.hash = arr[this.id];
-//			
-//			var point = this.getBBox(0);
-//			
-//			$('#map').next('.point').remove();
-//			
-//			$('#map').after($('<div />').addClass('point'));
-//			
-//			$('.point')
-//			.html(paths[arr[this.id]].name)
-//			.prepend($('<a />').attr('href', '#').addClass('close').text('Close'))
-//			.prepend($('<img />').attr('src', 'flags/'+arr[this.id]+'.png'))
-//			.css({
-//				left: point.x+(point.width/2)-80,
-//				top: point.y+(point.height/2)-20
-//			})
-//			.fadeIn();
-//			
-//		});
+		.mouseover(function(){
+			document.location.hash = arr[this.id];
+			
+			var point = this.getBBox(0);
+			
+			$('#map').next('.point').remove();
+			
+			$('#map').after($('<div />').addClass('point'));
+			
+			$('.point')
+			.html(paths[arr[this.id]].name)
+						.prepend($('<a />').attr('href', '#').addClass('triangle'))
+			//.prepend($('<a />').attr('href', '#').addClass('close').text('Close'))
+			//.prepend($('<img />').attr('src', 'flags/'+arr[this.id]+'.png'))
+			.css({
+				left: point.x+(point.width/2)-80,
+				top: point.y+(point.height/2)-20
+			})
+			.fadeIn();
+			
+		});
 		
-//		$('.point').find('.close').live('click', function(){
-//			var t = $(this),
-//				parent = t.parent('.point');
-//			
-//			parent.fadeOut(function(){
-//				parent.remove();
-//			});
-//			return false;
-//		});
+		$('.point').find('.close').live('click', function(){
+			var t = $(this),
+				parent = t.parent('.point');
+			
+			parent.fadeOut(function(){
+				parent.remove();
+			});
+			return false;
+		});
 		
 	}
 			
--- readme.md
+++ readme.md
@@ -3,7 +3,8 @@
 - Uses RafaelJs plugin 
 
 TODO:
-- Tooltips can be add
+- Tooltips can be add [Added]
+- City pictures will be add 
 - Mobile device support will be research
 
 
@@ -11,3 +12,7 @@ TODO:
 [See demo](http://selengora.com/test/harita)
 
 - jquery interactive Turkey Map, still in progress. Thanks..
+
+# Change Log - v1.1 2012/09/05
+
+- Tooltip added (Just names showing for now)
